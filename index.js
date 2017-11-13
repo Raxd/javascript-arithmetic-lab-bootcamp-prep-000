b78f8da194e4a3cd96ec5c0b489b5c3db637e9d4
rafael sanchez <augusto4nc@gmail.com>
2017-11-13 12:56:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return a+b
+}
+
+function subtract(a, b){
+  return a-b
+}
+
+function multiply(a, b){
+  return a*b
+}
+
+function divide(a, b){
+  return a / b
+}
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  n = parseInt('0x2328',10)
+  return parseInt(n)
+}
